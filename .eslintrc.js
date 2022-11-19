f4beb5d092c9d40d49648e05e84737fc5f2d3c4a
Akshat Saxena <akshats1010@gmail.com>
2022-11-19 17:18:08 +0530

    Lint changes and bcrpyto addition for authentication

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     commonjs: true,
     es2021: true,
   },
@@ -11,5 +11,12 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
   },
 };
--- app.js
+++ app.js
@@ -7,8 +7,9 @@ require('dotenv').config();
 const port = process.env.NODEJS_PORT || '8000';
 
 app.use(Express.json());
-app.use('/', apiRouter);
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening to requests on http://localhost:${port}`);
 });
